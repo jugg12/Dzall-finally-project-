2cabb2230e979bb338e521e0d1ee7a09f09cefab
jugg12 <maksim.blinov.001@mail.ru>
2022-12-15 18:14:11 +0300

    Updates

---
+++ precache-manifest.8f76d83eb65628a13155ce63c90bc0f3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "b137bbdcfff98d2ea0cb",
+    "url": "/Dzall-finally-project-/static/css/main.8e01cb6b.chunk.css"
+  },
+  {
+    "revision": "b137bbdcfff98d2ea0cb",
+    "url": "/Dzall-finally-project-/static/js/main.b137bbdc.chunk.js"
+  },
+  {
+    "revision": "ea883b44d5014734530d",
+    "url": "/Dzall-finally-project-/static/css/1.3925c0c3.chunk.css"
+  },
+  {
+    "revision": "ea883b44d5014734530d",
+    "url": "/Dzall-finally-project-/static/js/1.ea883b44.chunk.js"
+  },
+  {
+    "revision": "6591136beaf68c9c6149",
+    "url": "/Dzall-finally-project-/static/js/runtime~main.6591136b.js"
+  },
+  {
+    "revision": "1ce742c03376946268c079db3eb8e640",
+    "url": "/Dzall-finally-project-/static/media/8.1ce742c0.svg"
+  },
+  {
+    "revision": "fcd1a2ce667a062e3894d36db39b46e9",
+    "url": "/Dzall-finally-project-/static/media/9.fcd1a2ce.svg"
+  },
+  {
+    "revision": "7001761f721bb6c61dc431817f311881",
+    "url": "/Dzall-finally-project-/static/media/7.7001761f.svg"
+  },
+  {
+    "revision": "9843389e434dba2949158e6dafa7250a",
+    "url": "/Dzall-finally-project-/static/media/1.9843389e.svg"
+  },
+  {
+    "revision": "60ddc0945777a98cd6922ea9b5d1aea4",
+    "url": "/Dzall-finally-project-/static/media/2.60ddc094.svg"
+  },
+  {
+    "revision": "949aa62b56c4805dd04d024bb0e5f9b3",
+    "url": "/Dzall-finally-project-/static/media/3.949aa62b.svg"
+  },
+  {
+    "revision": "4a891fedd3066ea75ba238365c4488dd",
+    "url": "/Dzall-finally-project-/static/media/4.4a891fed.svg"
+  },
+  {
+    "revision": "07731a97b49b8bc5a3f1389eb73a09eb",
+    "url": "/Dzall-finally-project-/static/media/5.07731a97.svg"
+  },
+  {
+    "revision": "be3643e9f317be8e8c347fe03d8c5562",
+    "url": "/Dzall-finally-project-/static/media/6.be3643e9.svg"
+  },
+  {
+    "revision": "2debdbe9ac3ec48dc452ea2f1854885e",
+    "url": "/Dzall-finally-project-/static/media/1.2debdbe9.svg"
+  },
+  {
+    "revision": "22f6ca5042fd4f02602f81e57b5b2c91",
+    "url": "/Dzall-finally-project-/static/media/2.22f6ca50.svg"
+  },
+  {
+    "revision": "eb2b4727d71dfacb094596e38eb3cd2f",
+    "url": "/Dzall-finally-project-/static/media/3.eb2b4727.svg"
+  },
+  {
+    "revision": "83aec4e28739ccbc07e26ce8e4ba97d1",
+    "url": "/Dzall-finally-project-/static/media/4.83aec4e2.svg"
+  },
+  {
+    "revision": "d14046a4e01f0d80ee417db32ce71e1b",
+    "url": "/Dzall-finally-project-/static/media/11.d14046a4.svg"
+  },
+  {
+    "revision": "051a1ced07da829b74400538f26ab0cc",
+    "url": "/Dzall-finally-project-/static/media/arrowbottom.051a1ced.svg"
+  },
+  {
+    "revision": "f32264522608ed1bde06c386900bc606",
+    "url": "/Dzall-finally-project-/static/media/map.f3226452.svg"
+  },
+  {
+    "revision": "864fe919ae15a68241f60f0de57b287f",
+    "url": "/Dzall-finally-project-/static/media/map.864fe919.svg"
+  },
+  {
+    "revision": "c70333064074db21cfef0028b7f062dc",
+    "url": "/Dzall-finally-project-/index.html"
+  }
+];
